8aebaea71520d20134ecaa66a65e60e86fb14ee6
procombatpriv-95 <procombat.priv@gmail.com>
2025-09-22 23:27:59 +0100

    Update bouton.js

--- bouton.js
+++ bouton.js
@@ -37,7 +37,7 @@ function openBackgroundMenu(e){
   bgMenu.appendChild(label);
 
   const options = [
-    {name: 'Paysage', bg: 'https://wallpapers.com/images/hd/dark-nature-ecoszxkcqcayo73x.jpg'},
+    {name: 'Paysage', bg: 'https://cdn.pixabay.com/photo/2020/06/11/01/28/landscape-5284806_1280.jpg'},
     {name: 'Sombre',  bg: 'https://wallpapers.com/images/hd/dark-nature-ecoszxkcqcayo73x.jpg'},
     {name: 'Mode Chill', bg: 'https://img.tastelife.tv/assets/uploads/2022/01/New_Zealand_-_AMAZING_Beautiful_Nature_with_Relaxing_Music__Soundscapes_16x9.jpg'}
   ];

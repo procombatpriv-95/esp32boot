acb8603fe5c2b78913504d7c58079a963d77f609
procombatpriv-95 <procombat.priv@gmail.com>
2025-09-24 19:41:46 +0100

    Update bouton.js

--- bouton.js
+++ bouton.js
@@ -43,8 +43,8 @@ function openBackgroundMenu(e){
     {name: 'Paysage', bg: 'https://cdn.pixabay.com/photo/2020/06/11/01/28/landscape-5284806_1280.jpg'},
     {name: 'Sombre',  bg: 'https://wallpapers.com/images/hd/dark-nature-ecoszxkcqcayo73x.jpg'},
     {name: 'Mode Chill', bg: 'https://img.tastelife.tv/assets/uploads/2022/01/New_Zealand_-_AMAZING_Beautiful_Nature_with_Relaxing_Music__Soundscapes_16x9.jpg'},
-    {name: 'Plage', bg: 'https://izitour.com/media/blog/nha-trang-beaches.webp'},
-    {name: 'Lunaire', bg: 'https://static.vecteezy.com/ti/vecteur-libre/p1/37043037-magnifique-plage-paysage-avec-plein-lune-mer-nuit-paysage-vectoriel.jpg'}
+    {name: 'Plage', bg: 'https://eurolines.fr/wp-content/uploads/2022/07/plus-belles-plages.jpg'},
+    {name: 'Lunaire', bg: 'https://nativetransmission.wordpress.com/wp-content/uploads/2015/04/bloody-red-moon_p.jpg'}
   ];
 
   // Création de chaque option
